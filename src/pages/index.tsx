9e049c0327fdeebb55248a9e0eaca5eb9ba31b19
Oleg S <97077423+RobotSail@users.noreply.github.com>
2023-12-08 11:44:30 -0500

    initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,374 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
+import { ChangeEvent, useCallback, useEffect, useMemo, useState } from "react";
+import Markdown from "react-markdown";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+type Datapoint = {
+  backport_before: string;
+  backport_after: string;
+  upstream_before: string;
+  upstream_after: string;
+  backport_patch: string;
+  upstream_patch: string;
+  generated_patch: string;
+  extracted_patch: string;
+};
+
+type FieldName = keyof Datapoint;
+
+// this function returns the field names present in a given datapoint
+function getPresentFieldNames(dp: Partial<Datapoint>): FieldName[] {
+  const completeDatapoint: Datapoint = {
+    backport_after: "",
+    backport_before: "",
+    backport_patch: "",
+    extracted_patch: "",
+    generated_patch: "",
+    upstream_after: "",
+    upstream_before: "",
+    upstream_patch: "",
+  };
+  const containedFields: FieldName[] = [];
+  for (const key of Object.keys(completeDatapoint)) {
+    if (key in dp) {
+      containedFields.push(key as FieldName);
+    }
+  }
+
+  return containedFields;
+}
+
+function useDatapoints() {
+  const [datapoints, setDatapoints] = useState<Partial<Datapoint>[]>([]);
+  const [index, setIndex] = useState(0);
+  const [selectedFields, setSelectedFields] = useState<FieldName[]>([]);
+  const [fieldToAdd, setFieldToAdd] = useState<FieldName | undefined>(
+    undefined,
+  );
+
+  const updateDatapoints = useCallback((data: Partial<Datapoint>[]) => {
+    setDatapoints(data);
+    setIndex(0);
+  }, []);
+
+  // reset the selected fields whenever a new datapoint is loaded
+  useEffect(() => {
+    if (datapoints.length === 0) {
+      return;
+    }
+    const availableFields = getPresentFieldNames(datapoints[index]);
+    // do nothing
+    if (availableFields.length === 0) {
+      setSelectedFields([]);
+      return;
+    }
+    setSelectedFields([availableFields[0]]);
+  }, [datapoints, index]);
+
+  const changeIndex = useCallback(
+    (operation: "increment" | "decrement") => {
+      switch (operation) {
+        case "decrement":
+          if (0 < index) {
+            setIndex((i) => i - 1);
+          }
+          break;
+        case "increment":
+          if (index < datapoints.length - 1) {
+            setIndex((i) => i + 1);
+          }
+      }
+    },
+    [datapoints.length, index],
+  );
+
+  const availableFields = useMemo(() => {
+    if (datapoints.length === 0) {
+      return [];
+    }
+    const allFieldNames = getPresentFieldNames(datapoints[index]);
+    return allFieldNames.filter((fn) => !selectedFields.includes(fn));
+  }, [datapoints, index, selectedFields]);
+
+  const handleFieldSelectChange = useCallback(
+    (e: ChangeEvent<HTMLSelectElement>) => {
+      const candidate = e.target.value;
+      console.log("received change event: ", candidate);
+      if (candidate.length === 0) {
+        console.log("candidate is length 0");
+        return;
+      }
+      setFieldToAdd(candidate as FieldName);
+    },
+    [],
+  );
+
+  const handleAddFieldClick = useCallback(() => {
+    // ignore invalid entry
+    console.log("received click event");
+    if (!fieldToAdd) {
+      if (availableFields.length === 0) {
+        return;
+      }
+      const defaultField = availableFields[0];
+      setSelectedFields((sf) => [...sf, defaultField]);
+      setFieldToAdd(undefined);
+      return;
+    }
+    if (fieldToAdd.length === 0) {
+      console.log("invalid fieldname to add: ", fieldToAdd);
+      return;
+    }
+    // add the field to the list of selected fields, clear the entry screen
+    setSelectedFields((sf) => [...sf, fieldToAdd]);
+    setFieldToAdd(undefined);
+  }, [availableFields, fieldToAdd]);
+
+  const removeFieldName = (fn: FieldName) => {
+    setSelectedFields((fields) => fields.filter((f) => f !== fn));
+  };
+
+  return {
+    datapoints,
+    handleFieldSelectChange,
+    handleAddFieldClick,
+    updateDatapoints,
+    index,
+    changeIndex,
+    selectedFields,
+    removeFieldName,
+    availableFields,
+  };
+}
+type FileDiffViewerProps = {
+  data: string;
+  fieldName: string;
+  onCloseClick: () => void;
+};
+function FileDiffViewer({
+  data,
+  fieldName,
+  onCloseClick,
+}: FileDiffViewerProps) {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div>
+      <div className="fixed -translate-y-8">
+        <div className="flex flex-row  space-x-2 px-2 bg-gray-300">
+          <button
+            onClick={onCloseClick}
+            className="bg-red-400 hover:bg-red-300 px-2"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            X
+          </button>
+          <h1 className="">
+            Viewing: <span className="font-bold">{fieldName}</span>
+          </h1>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div
+        className="border-gray-200 overflow-y-scroll bg-gray-50 border p-4"
+        style={{
+          width: "726px",
+          height: "1024px",
+        }}
+      >
+        <Markdown>
+          {`\`\`\`diff
+${data}
+\`\`\``}
+        </Markdown>
       </div>
+    </div>
+  );
+}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+type FileViewerContainerProps = {
+  datapoint: Partial<Datapoint>;
+  selectedFields: FieldName[];
+  removeField: (fieldName: FieldName) => void;
+  availableFields: FieldName[];
+  onFieldSelectChange: (e: ChangeEvent<HTMLSelectElement>) => void;
+  onAddFieldClick: () => void;
+};
+// function FileViewerContainer({
+//   datapoint,
+//   selectedFields,
+//   availableFields,
+//   removeField,
+//   onAddFieldClick,
+//   onFieldSelectChange,
+// }: FileViewerContainerProps) {
+//   return (
+//     <div className="flex flex-row max-h-[80%] overflow-x-scroll ">
+//       {selectedFields.map((sf, i) => {
+//         return (
+//           <div key={`field-viewer-${i}`}>
+//             <FileDiffViewer
+//               data={datapoint[sf]!}
+//               fieldName={sf}
+//               onCloseClick={() => removeField(sf)}
+//             />
+//           </div>
+//         );
+//       })}
+//       {availableFields.length > 0 && (
+//         <div className="w-48 h-fit text-center border border-black">
+//           <h1 className="text-lg">Add Field View</h1>
+//           <select onChange={onFieldSelectChange}>
+//             {availableFields.map((af, i) => (
+//               <option value={af} key={`available-fieldname-${i}`}>
+//                 {af}
+//               </option>
+//             ))}
+//           </select>
+//           <button className="bg-gray-100" onClick={onAddFieldClick}>
+//             Add a File
+//           </button>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+function FileViewerContainer({
+  datapoint,
+  selectedFields,
+  availableFields,
+  removeField,
+  onAddFieldClick,
+  onFieldSelectChange,
+}: FileViewerContainerProps) {
+  return (
+    <div className="flex overflow-x-auto whitespace-nowrap">
+      {selectedFields.map((sf, i) => {
+        return (
+          <div key={`field-viewer-${i}`} className="flex-shrink-0">
+            <FileDiffViewer
+              data={datapoint[sf]!}
+              fieldName={sf}
+              onCloseClick={() => removeField(sf)}
+            />
+          </div>
+        );
+      })}
+      {availableFields.length > 0 && (
+        <div className="w-48 h-fit text-center border border-black inline-block">
+          <h1 className="text-lg">Add Field View</h1>
+          <select onChange={onFieldSelectChange}>
+            {availableFields.map((af, i) => (
+              <option value={af} key={`available-fieldname-${i}`}>
+                {af}
+              </option>
+            ))}
+          </select>
+          <button className="bg-gray-100" onClick={onAddFieldClick}>
+            Add a File
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+type DatasetControllerProps = {
+  handleFileChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  index: number;
+  datasetLength: number;
+  onPreviousClick: () => void;
+  onNextClick: () => void;
+};
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+function DatasetController(props: DatasetControllerProps) {
+  return (
+    <div className="flex flex-col justify-center mb-8">
+      <input
+        type="file"
+        accept="application/json"
+        onChange={props.handleFileChange}
+      />
+      <div className="mt-4 text-center">
+        Viewing Datapoint {Math.min(props.index + 1, props.datasetLength)} out
+        of {props.datasetLength}
       </div>
+      <div className="mx-auto space-x-4 mt-2">
+        <button
+          className="bg-gray-100 px-2 py-1 border border-black rounded-sml"
+          onClick={props.onPreviousClick}
+        >
+          Previous
+        </button>
+        <button
+          className="bg-gray-100 px-2 py-1 border border-black rounded-sml"
+          onClick={props.onNextClick}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default function Home() {
+  const {
+    changeIndex,
+    datapoints,
+    index,
+    updateDatapoints,
+    selectedFields,
+    removeFieldName,
+    availableFields,
+    handleAddFieldClick,
+    handleFieldSelectChange,
+  } = useDatapoints();
+  const [error, setError] = useState("");
+  const handleFileChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      const file = event.target.files![0];
+      const reader = new FileReader();
+      reader.onload = function (event) {
+        try {
+          const data = JSON.parse(event.target!.result as string);
+          if (!Array.isArray(data)) {
+            setError(
+              "Provided file is not in list format. Please see instructions for file format.",
+            );
+            return;
+          }
+          // successful parse
+          updateDatapoints(data as Datapoint[]);
+          setError("");
+        } catch (e) {
+          console.log("got error parsing file:", e);
+          setError(e as string);
+          return;
+        }
+      };
+      reader.readAsText(file);
+    },
+    [updateDatapoints],
+  );
+  return (
+    <main
+      className={`flex h-screen w-screen  flex-col items-center  p-24 ${inter.className}`}
+    >
+      {error.length > 0 && (
+        <div className="bg-red-400 text-white p-4">{error}</div>
+      )}
+      <DatasetController
+        datasetLength={datapoints.length}
+        index={index}
+        handleFileChange={handleFileChange}
+        onNextClick={() => changeIndex("increment")}
+        onPreviousClick={() => changeIndex("decrement")}
+      />
+      <FileViewerContainer
+        datapoint={datapoints[index]}
+        removeField={removeFieldName}
+        selectedFields={selectedFields}
+        availableFields={availableFields}
+        onAddFieldClick={handleAddFieldClick}
+        onFieldSelectChange={handleFieldSelectChange}
+      />
     </main>
-  )
+  );
 }
